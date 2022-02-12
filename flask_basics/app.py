232c3e6a740e46520aabc0c4ac0728d8927a1545
Aman Verma <aman.kash94@gmail.com>
2022-02-12 21:10:18 +0530

    route variables

--- flask_basics/app.py
+++ flask_basics/app.py
@@ -8,9 +8,30 @@ def index():
     return '<h1>Hello, World!</h1>'
 
 
-@app.route('/home')
-def home():
-    return '<h1>Home Page</h1>'
+# @app.route('/home')
+# def home():
+#     return '<h1>Home Page</h1>'
+""" 
+string
+(default) accepts any text without a slash
+
+int
+accepts positive integers
+
+float
+accepts positive floating point values
+
+path
+like string but also accepts slashes
+
+uuid
+accepts UUID strings """
+
+
+@app.route('/home', methods=['POST', 'GET'], defaults={'name': 'John'})
+@app.route('/home/<string:name>', methods=['POST', 'GET'])
+def home(name):
+    return '<h1>{} is on Home Page</h1>'.format(name)
 
 
 @app.route('/json')
