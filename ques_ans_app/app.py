476c97c76912813af7bedff839e706b20af59c24
Aman Verma <aman.kash94@gmail.com>
2022-02-19 10:13:04 +0530

    updated nav bar links

--- ques_ans_app/app.py
+++ ques_ans_app/app.py
@@ -14,14 +14,15 @@ def close_db(error):
 
 
 def get_current_user():
-    # user = None
     user_result = None
+
     if 'user' in session:
         user = session['user']
 
         db = get_db()
         user_cur = db.execute(
-            'select id,name,password from users where name= ?', [user])
+            'select id, name, password, expert, admin from users where name = ?',
+            [user])
         user_result = user_cur.fetchone()
 
     return user_result
@@ -54,7 +55,7 @@ def register():
         """ return '<h1>Name: {}, Password: {}</h1>'.format(
             request.form["name"], request.form["password"]) """
 
-    return render_template('register.html')
+    return render_template('register.html', user=user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -77,42 +78,41 @@ def login():
             return '<h1>The password is correct</h1>'
         else:
             return '<h1>The password is incorrect</h1>'
-    return render_template('login.html')
+    return render_template('login.html', user=user)
 
 
 @app.route('/question')
 def question():
     user = get_current_user()
-    return render_template('question.html')
+    return render_template('question.html', user=user)
 
 
 @app.route('/answer')
 def answer():
     user = get_current_user()
-    return render_template('answer.html')
+    return render_template('answer.html', user=user)
 
 
 @app.route('/ask')
 def ask():
     user = get_current_user()
-    return render_template('ask.html')
+    return render_template('ask.html', user=user)
 
 
 @app.route('/unanswered')
 def unanswered():
     user = get_current_user()
-    return render_template('unanswered.html')
+    return render_template('unanswered.html', user=user)
 
 
 @app.route('/users')
 def users():
     user = get_current_user()
-    return render_template('users.html')
+    return render_template('users.html', user=user)
 
 
 @app.route('/logout')
 def logout():
-    user = get_current_user()
     # return render_template('home.html')
     session.pop('user', None)
     return redirect(url_for('index'))
