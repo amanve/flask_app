0cffc9def91f3e8485b80842b8b7d60ad6f8f7d4
Aman Verma <aman.kash94@gmail.com>
2022-03-07 01:13:08 +0530

    create tables

--- flask_sqlalchemy/app.py
+++ flask_sqlalchemy/app.py
@@ -11,6 +11,24 @@ db = SQLAlchemy(app)
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
+    # Create a string
+    def __repr__(self):
+        return '<Test %r>' % self.name
+
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30))
+    email = db.Column(db.String(30))
+    join_date = db.Column(db.DateTime)
+
+    # Create a string
+    """ Returns representation of the object in Database """
+
+    def __repr__(self):
+        return '<Member %r>' % self.name
+
 
 if __name__ == '__main__':
     app.run()
