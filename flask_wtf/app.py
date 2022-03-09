03bcd0d0e2eb06d0ff5d3cfded95f533ae743595
Aman Verma <aman.kash94@gmail.com>
2022-03-10 00:08:29 +0530

    wtf flask

--- flask_wtf/app.py
+++ flask_wtf/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config[
@@ -8,17 +9,20 @@ app.config[
 
 
 class LoginForm(FlaskForm):
-  username = StringField('username')
-  password = PasswordField('password')
+  username = StringField(label=('username:'),
+                         validators=[DataRequired(message='*Required')])
+  password = PasswordField(label=('password:'),
+                           validators=[DataRequired('*Required')])
+  submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  form = LoginForm()
-  if form.validate_on_submit():
-    return f'<h1>Username:{form.username.data} Password:{form.password.data}</h1>'
+  this_form = LoginForm()
+  if this_form.validate_on_submit():
+    return f'<h1>Username:{this_form.username.data} Password:{this_form.password.data}</h1>'
 
-  return render_template('index.html', form=form)
+  return render_template('index.html', form=this_form)
 
 
 if __name__ == '__main__':
