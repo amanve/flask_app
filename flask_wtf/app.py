7a9731f0728368ea1593c2970c279978e0bced13
Aman Verma <aman.kash94@gmail.com>
2022-03-09 09:34:43 +0530

    form creation

--- flask_wtf/app.py
+++ flask_wtf/app.py
@@ -3,12 +3,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 
 app = Flask(__name__)
-app.config['SECRETKEY'] = 'secretkey!'
+app.config['SECRET_KEY'] = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
 
 
 class LoginForm(FlaskForm):
     username = StringField('username')
     password = PasswordField('password')
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    form=LoginForm()
+
+    return render_template('index.html',form=form)
 
 
 if __name__ == '__main__':
