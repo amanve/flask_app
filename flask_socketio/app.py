33eafa1abc30f4df638df9475a8e5130c3527493
Aman Verma <aman.kash94@gmail.com>
2022-03-16 23:25:51 +0530

    initial socketio setup

--- flask_socketio/app.py
+++ flask_socketio/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecret!'
 
+socketIO = SocketIO(app)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
 if __name__ == '__main__':
-  app.run()
+  socketIO.run(app)
