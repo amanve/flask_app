9eb796bcb704469045b065d0ea30cdcc2cf6a11a
Aman <aman.kash94@gmail.com>
2022-03-24 11:59:21 +0530

    setup register page

--- forum_app/app.py
+++ forum_app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
+from flask_security.forms import RegisterForm
+from wtforms import StringField
 
 app = Flask(__name__)
 
@@ -40,8 +42,13 @@ class User(db.Model, UserMixin):
                             backref=db.backref('users', lazy='dynamic'))
 
 
+class ExtendRegisterForm(RegisterForm):
+    name = StringField('Name')
+    username = StringField('Username')
+
+
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+security = Security(app, user_datastore, register_form=ExtendRegisterForm)
 
 
 @app.route('/')
@@ -57,13 +64,3 @@ def profile():
 @app.route('/thread')
 def thread():
     return render_template('thread.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
