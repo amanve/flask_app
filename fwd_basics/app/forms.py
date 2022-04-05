562c5222ebfbd6fd43b2fd700d14c1fc6275898d
Aman <aman.kash94@gmail.com>
2022-04-05 16:10:50 +0530

    user logins

--- fwd_basics/app/forms.py
+++ fwd_basics/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, BooleanField, SubmitField)
-from wtforms.validators import InputRequired
+from wtforms.validators import (InputRequired, Email, EqualTo, ValidationError)
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    password2 = PasswordField('Retype Password',
+                              validators=[InputRequired(),
+                                          EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter(User.username == username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter(User.email == email.data).first()
+        if user is not None:
+            raise ValidationError('Email already registered.')
